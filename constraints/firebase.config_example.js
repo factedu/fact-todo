e5134dde0fefdad333ec222dd90d613eca622eb0
Ravi S. Singh <factedu@gmail.com>
2020-05-10 08:11:22 +0530

    added config example

---
+++ constraints/firebase.config_example.js
@@ -0,0 +1,15 @@
+/**
+ * update your firebase config here and rename filename to firebase.config.js
+ */
+
+export const firebaseConfig = {
+    apiKey: "<API-KEY>",
+    authDomain: "<authDomain>",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+
